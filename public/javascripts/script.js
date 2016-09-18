ff8e32b51da9bb2816d8ac82a10821ad288a81a4
Michael Gillett <mgillett54@gmail.com>
2016-09-18 11:16:40 -0400

    More UI updates

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,18 @@
 var ready = function() {
   $('.thumb a').click(function() {
     artist_name = $(this).data('artist');
-    $.post('/lyric', {artist: artist_name}, function(data) {
-      lyrics = data.lyrics
-      console.log(lyrics)
-      $('.lyrics-container').html(lyrics)
-    })
+    if(typeof artist_name != 'undefined') {
+      $(".lyrics .subtitle").fadeIn(600)
+      $(".lyrics .subtitle").html('Training...')
+      $('html, body').animate({
+        scrollTop: $(".lyrics .subtitle").offset().top
+      }, 1000);
+      $.post('/lyric', {artist: artist_name}, function(data) {
+        lyrics = data.lyrics.replace('*BREAK*', '\n')
+        $('.lyrics .subtitle').html(artist_name.replace('_', ' '))
+        $('.lyrics .container').html(lyrics)
+      })
+    }
   })
 }
 

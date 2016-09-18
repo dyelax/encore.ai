150679af492ffd51874961a608a36e4bb79711d9
Michael Gillett <mgillett54@gmail.com>
2016-09-18 11:16:40 -0400

    More UI updates

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var SSH = require('simple-ssh')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,16 +8,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/lyric', function(req, res, next) {
-  artist = 'kanye_west'
-  var exec = require('child_process').exec;
-  var cmd = 'python code/runner.py -l save/models/kanye_west/model.ckpt -a kanye_west -t'
+  artist = req.body.artist
 
-  exec(cmd, function(error, stdout, stderr) {
-    raw_response = stdout.trim().split('\n')
-    lyrics = raw_response[raw_response.length - 1]
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({lyrics: lyrics}));
+  var cmd = 'cd /home/mjc/Research/encore.ai/code; python runner.py -l ../save/models/'+artist+'/model.ckpt -a '+artist+' -t'
+
+  var ssh = new SSH({
+    host: '138.16.160.16',
+    port: 5556,
+    user: 'mjc',
+    pass: 'L@xin4life'
   });
+
+  ssh.exec(cmd, {
+    error: function(err, resp) {
+      console.log(error)
+    },
+    out: function(err, resp) {
+      if(err) {
+        throw err;
+      }
+      console.log(resp)
+      raw_response = resp.trim().split('\n')
+      console.log(raw_response)
+      lyrics = raw_response[raw_response.length - 1]
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({lyrics: lyrics}));
+    }
+  }).start();
 });
 
 module.exports = router;

735bebca3b2f844fc2a3911ef97ba5263c742124
Matt Cooper <matthew_cooper@brown.edu>
2016-09-19 10:10:25 -0400

    cleaned up constants.py

--- code/constants.py
+++ code/constants.py
@@ -1,6 +1,31 @@
 from os import makedirs
 from os.path import join, exists
 
+##
+# Network
+##
+
+# Size of LSTM cell hidden states and word embeddings.
+CELL_SIZE = 256
+# Number of LSTM layers
+NUM_LAYERS = 2
+
+##
+# Training
+##
+
+# Learning rate for training.
+L_RATE = 0.002
+# Number of sequences in a training batch.
+BATCH_SIZE = 50
+# Length of sequence to train on,
+# eg., seq_len=5 => inputs='The quick brown fox jumps', targets='quick brown fox jumps over'.
+SEQ_LEN = 5
+
+##
+# Data
+##
+
 def get_dir(directory):
     """
     Creates the given directory if it does not exist.
@@ -11,47 +36,24 @@ def get_dir(directory):
     if not exists(directory):
         makedirs(directory)
     return directory
-
-# CELL_SIZE = 256
-# NUM_LAYERS = 2
-#
-# BATCH_SIZE = 50
-# SEQ_LEN = 5
-#
-# MODEL_SAVE_FREQ =  1000
-#
-# L_RATE = 0.002
-
-
-
-CELL_SIZE = 256
-NUM_LAYERS = 2
-
-BATCH_SIZE = 50
-SEQ_LEN = 5
-
-MODEL_SAVE_FREQ = 5000
-
-L_RATE = 0.002
-
-
-# Data
-
 def set_save_name(name):
     """
     Edits all constants dependent on SAVE_NAME.
 
     @param name: The new save name.
     """
-    global SAVE_NAME, MODEL_SAVE_DIR, SAMPLE_SAVE_DIR
+    global SAVE_NAME, MODEL_SAVE_DIR
 
     SAVE_NAME = name
     MODEL_SAVE_DIR = get_dir(join(SAVE_DIR, 'models/', SAVE_NAME))
-    SAMPLE_SAVE_DIR = get_dir(join(join(SAVE_DIR, 'samples'), SAVE_NAME))
 
 
+# Directory in which to save content
 SAVE_DIR = get_dir('../save/')
+# subdirectory of SAVE_DIR/* to differentiate between runs/models.
 SAVE_NAME = 'default/'
-
-SAMPLE_SAVE_DIR = get_dir(join(join(SAVE_DIR, 'samples'), SAVE_NAME))
+# Directory in which to save trained models.
 MODEL_SAVE_DIR = get_dir(join(join(SAVE_DIR, 'models'), SAVE_NAME))
+
+# How often to save the model, in # steps.
+MODEL_SAVE_FREQ = 5000

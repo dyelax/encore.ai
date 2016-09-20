22a474961ffc3397bb4e855b646e5cd0ba867782
Matt Cooper <matthew_cooper@brown.edu>
2016-09-19 22:59:56 -0400

    turned back on sampling

--- code/LSTMModel.py
+++ code/LSTMModel.py
@@ -151,7 +151,7 @@ class LSTMModel:
                                                 name='train_op')
 
 
-    def generate(self, num_out=200, prime=None, sample=False):
+    def generate(self, num_out=200, prime=None, sample=True):
         """
         Generate a sequence of text from the trained model.
 
